2c95c81e31221784ea5d55a462e5bb6a718fd22d
muzafferovun <83140937+muzafferovun@users.noreply.github.com>
2021-04-25 00:05:55 +0300

    Add files via upload

---
+++ intro/src/intro/Main.java
@@ -0,0 +1,11 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String btnmetin="buton yazýsý";
+		System.out.println("hello world\n"+btnmetin);
+	}
+
+}
