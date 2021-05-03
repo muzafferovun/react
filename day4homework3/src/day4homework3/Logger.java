df53a8a0f6e689ace16ef1e68ef04efa147e2f93
muzafferovun <83140937+muzafferovun@users.noreply.github.com>
2021-05-03 21:43:50 +0300

    Delete day4homework3 directory

--- day4homework3/src/day4homework3/Logger.java
+++
@@ -1,6 +0,0 @@
-package day4homework3;
-
-public interface Logger {
-	void log(String message);
-
-}
--- day4homework3/src/day4homework3/Utils.java
+++
@@ -1,8 +0,0 @@
-package day4homework3;
-
-public class Utils {
-	public static void RunLoggers(Logger[] loggers,String message) {
-		for(Logger logger:loggers)	logger.log(message);
-
-	}
-}
